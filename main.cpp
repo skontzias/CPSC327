fff5ae7ef37192351994d06ca5b282221da3f33d
skontzias <skontzias@yahoo.com>
2021-02-06 19:41:02 -0800

    Project 1

---
+++ main.cpp
@@ -0,0 +1,9 @@
+//#includes are ALWAYS at top of file
+#include <iostream>	//iostream is where std::cout and std::endl are defined
+#include "myfunc.h"
+
+int main()
+{
+	std::cout<<func()<<std::endl;
+	return 0;
+}
---
+++ myfunc.cpp
@@ -0,0 +1,8 @@
+//My func
+#include "myfunc.h"
+
+std::string func()
+{
+	//hello world
+	return "hello world";
+}
